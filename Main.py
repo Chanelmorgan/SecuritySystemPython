beee3e30cc18d4987a576382ea5086cb0179a407
Chanel Morgan <chanelmorgan05@icloud.com>
2024-04-17 08:49:13 +0100

    Drawing rectangle around detected faces

--- Main.py
+++ Main.py
@@ -10,12 +10,16 @@ body_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_fullbo
 
 while True:
     _, frame = cap.read()
-    cv2.imshow("Camera", frame)
 
     # Classifiers require a greyscale image
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
+    # Drawing rectangles on the frame
+    for(x, y, width, height) in faces:
+        cv2.rectangle(frame, (x, y), (x+width, y + height), (255, 0, 0), 3)# BGR
+
+    cv2.imshow("Camera", frame)
 
     if cv2.waitKey(1) == ord('q'):
         break
