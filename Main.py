65483a4afbf8a5e79cde1b41363e53392c17d8d7
Chanel Morgan <chanelmorgan05@icloud.com>
2024-04-17 12:27:22 +0100

    Commenting the finsihed code

--- Main.py
+++ Main.py
@@ -8,7 +8,7 @@ cap = cv2.VideoCapture(0)
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 body_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_fullbody.xml")
 
-detection = True
+detection = False
 detection_stopped_time = None
 timer_started = False
 SECONDS_TO_RECORD_AFTER_DETECTION = 5
@@ -28,14 +28,17 @@ while True:
     # Only start recording once a body or face detected
     if len(faces) + len(bodies) > 0:
         if detection:
+            # check if we are already recording
             timer_started = False
         else:
+            # if we was not recording we need to start recording because there is a face or body
             detection = True
             current_time = datetime.datetime.now().strftime("%d-%m-%Y-%H-%M-%S")
             out = cv2.VideoWriter(f"{current_time}.mp4", fourcc, 20, frame_size)
             print("Started Recording!")
     elif detection:
         if timer_started:
+            # Check if the timer has expired
             if time.time() - detection_stopped_time >= SECONDS_TO_RECORD_AFTER_DETECTION:
                 detection = False
                 timer_started = False
@@ -45,7 +48,8 @@ while True:
             else:
                 timer_started = True
                 detection_stopped_time = time.time()
-    out.write(frame)
+    if detection:
+        out.write(frame)
 
     # Drawing rectangles on the frame
     # for(x, y, width, height) in faces:
@@ -56,6 +60,7 @@ while True:
     if cv2.waitKey(1) == ord('q'):
         break
 
+# releasing all resources
 out.release()
 cap.release()
 cv2.destoryAllWindows()
