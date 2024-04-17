3818557afd40bbd61aeae6d068972510e29e6f3c
Chanel Morgan <chanelmorgan05@icloud.com>
2024-04-17 08:45:27 +0100

    Getting classifiers

--- Main.py
+++ Main.py
@@ -4,9 +4,18 @@ import datetime
 
 cap = cv2.VideoCapture(0)
 
+# using prebuilt in classifier
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+body_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_fullbody.xml")
+
 while True:
     _, frame = cap.read()
     cv2.imshow("Camera", frame)
+
+    # Classifiers require a greyscale image
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
 
     if cv2.waitKey(1) == ord('q'):
         break
