35d80f44e863cf1f2ffc057e79436f81edcd09f5
Chanel Morgan <chanelmorgan05@icloud.com>
2024-04-17 08:58:56 +0100

    Cpaturing video

--- Main.py
+++ Main.py
@@ -8,22 +8,41 @@ cap = cv2.VideoCapture(0)
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 body_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_fullbody.xml")
 
+
+recording = True
+
+frame_size = (int(cap.get(3)), int(cap.get(4)))  # Corrected syntax
+fourcc = cv2.VideoWriter_fourcc(*"mp4v")  # Corrected function name
+out = cv2.VideoWriter("video.mp4", fourcc, 20, frame_size)
+
+
 while True:
     _, frame = cap.read()
 
     # Classifiers require a greyscale image
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    bodies = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+
+    # Only start recording once a body or face detected
+    if len(faces) + len(bodies) > 0:
+        recording = True
+
+    out.write(frame)
+
+
 
     # Drawing rectangles on the frame
-    for(x, y, width, height) in faces:
-        cv2.rectangle(frame, (x, y), (x+width, y + height), (255, 0, 0), 3)# BGR
+    # for(x, y, width, height) in faces:
+    #     cv2.rectangle(frame, (x, y), (x+width, y + height), (255, 0, 0), 3) #BGR
 
     cv2.imshow("Camera", frame)
 
     if cv2.waitKey(1) == ord('q'):
         break
 
+out.release()
 cap.release()
 cv2.destoryAllWindows()
 
